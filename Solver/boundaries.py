a39d905f2d67bed3186c9ed510c94b5d03f83714
Stelio Henrique <steliohlopes@gmail.com>
2023-07-19 16:33:02 -0300

    [clean] format boundaries.py

--- Solver/boundaries.py
+++ Solver/boundaries.py
@@ -2,13 +2,20 @@ from dolfin import *
 from ..ProblemInputs import Inputs
 from ..PreProcessing.mesh import Mesh
 
+
 class Boundaries:
-    def __init__(self,input = Inputs(),mesh = Mesh()):
-        #TODO Fazer para 2D verificando o tamanho do elemento
+    def __init__(self, inletCondition, input=Inputs(), mesh=Mesh()):
+        # TODO Fazer para 2D verificando o tamanho do elemento
+        self.inletCondition = inletCondition
         self.bcs = []
 
         ## No slip Boundaries
         for sub in input.noSlipBCs:
-            self.bcs.append(DirichletBC(mesh.functionSpace.sub(0),Constant((0.0,0.0,0.0)),mesh.mf,mesh.subdomains[sub]))
-
-
+            self.bcs.append(
+                DirichletBC(
+                    mesh.functionSpace.sub(0),
+                    Constant((0.0, 0.0, 0.0)),
+                    mesh.mf,
+                    mesh.subdomains[sub],
+                )
+            )
