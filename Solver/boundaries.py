a5b74662321258dae00783c0d16d2ff1af134ca3
Stelio Henrique <steliohlopes@gmail.com>
2023-07-25 17:32:55 -0300

    [add] Pressure&Velocity inletCondition

--- Solver/boundaries.py
+++ Solver/boundaries.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append("..")
 from dolfin import *
 from ProblemInputs import Inputs
@@ -7,6 +8,11 @@ from PreProcessing.mesh import FiniteElementMesh
 
 class Boundaries:
     def __init__(self, inletCondition, input=Inputs(), mesh=FiniteElementMesh()):
+        ###########################################
+        # inletCondition = 0 -> Constant inlet Pressure Condition
+        # inletCondition = 1 -> Constant inlet X Velocity  Condition
+        # inletCondition = 2 -> fully developed flow inlet X Velocity  Condition
+
         # TODO Fazer para 2D verificando o tamanho do elemento
         self.inletCondition = inletCondition
         self.bcs = []
@@ -21,3 +27,26 @@ class Boundaries:
                     mesh.subdomains[sub],
                 )
             )
+
+        if self.inletCondition == 0:
+            input.pressureBC()
+            self.Pin = input.Pin
+            self.inletBCs = input.inletBCs
+            # TODO Adicionar condições no Solver para Inlet Pressure
+
+        elif self.inletCondition == 1:
+            input.VelocityBC()
+            self.Uin = input.Uin
+            self.inletBCs = input.inletBCs
+            for sub in self.inletBCs:
+                self.bcs.append(
+                    DirichletBC(
+                        mesh.functionSpace.sub(0),
+                        Constant((self.Uin, 0.0, 0.0)),
+                        mesh.mf,
+                        mesh.subdomains[sub],
+                    )
+                )
+        
+        
+
