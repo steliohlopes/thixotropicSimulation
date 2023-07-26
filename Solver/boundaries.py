31b16d8443d43017d6a788c811cfcf2e1c7c163a
Steliolopes <steliohlopes@gmail.com>
2023-07-26 10:10:01 -0300

    [fix] better code boundary dimensions

--- Solver/boundaries.py
+++ Solver/boundaries.py
@@ -13,70 +13,46 @@ class Boundaries:
         # inletCondition = 1 -> Constant inlet X Velocity  Condition
         # inletCondition = 2 -> fully developed flow inlet X Velocity  Condition
 
+        # TODO Fazer para 2D verificando o tamanho do elemento
         self.inletCondition = inletCondition
         self.bcs = []
 
         if mesh.Dim == 3:
-            ## No slip Boundaries
-            for sub in input.noSlipBCs:
-                self.bcs.append(
-                    DirichletBC(
-                        mesh.functionSpace.sub(0),
-                        Constant((0.0, 0.0, 0.0)),
-                        mesh.mf,
-                        mesh.subdomains[sub],
-                    )
-                )
-
-            if self.inletCondition == 0:
-                input.pressureBC()
-                self.Pin = input.Pin
-                self.inletBCs = input.inletBCs
-
-            elif self.inletCondition == 1:
-                input.VelocityBC()
-                self.Uin = input.Uin
-                self.inletBCs = input.inletBCs
-                for sub in self.inletBCs:
-                    self.bcs.append(
-                        DirichletBC(
-                            mesh.functionSpace.sub(0),
-                            Constant((self.Uin, 0.0, 0.0)),
-                            mesh.mf,
-                            mesh.subdomains[sub],
-                        )
-                    )
-
+            noSlipVector = Constant((0.0, 0.0, 0.0))
+            UinVector = Constant((self.Uin, 0.0, 0.0))
         elif mesh.Dim == 2:
-            ## No slip Boundaries
-            for sub in input.noSlipBCs:
+            noSlipVector = Constant((0.0, 0.0))
+            UinVector = Constant((self.Uin, 0.0))
+
+        ## No slip Boundaries
+        for sub in input.noSlipBCs:
+            self.bcs.append(
+                DirichletBC(
+                    mesh.functionSpace.sub(0),
+                    noSlipVector,
+                    mesh.mf,
+                    mesh.subdomains[sub],
+                )
+            )
+
+        if self.inletCondition == 0:
+            input.pressureBC()
+            self.Pin = input.Pin
+            self.inletBCs = input.inletBCs
+
+        elif self.inletCondition == 1:
+            input.VelocityBC()
+            self.Uin = input.Uin
+            self.inletBCs = input.inletBCs
+            for sub in self.inletBCs:
                 self.bcs.append(
                     DirichletBC(
                         mesh.functionSpace.sub(0),
-                        Constant((0.0, 0.0)),
+                        UinVector,
                         mesh.mf,
                         mesh.subdomains[sub],
                     )
                 )
-
-            if self.inletCondition == 0:
-                input.pressureBC()
-                self.Pin = input.Pin
-                self.inletBCs = input.inletBCs
-
-            elif self.inletCondition == 1:
-                input.VelocityBC()
-                self.Uin = input.Uin
-                self.inletBCs = input.inletBCs
-                for sub in self.inletBCs:
-                    self.bcs.append(
-                        DirichletBC(
-                            mesh.functionSpace.sub(0),
-                            Constant((self.Uin, 0.0)),
-                            mesh.mf,
-                            mesh.subdomains[sub],
-                        )
-                    )
         
         
 
