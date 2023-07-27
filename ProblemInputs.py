e83f33cad60bf1fb0081cb84ac4f8b07798d3213
Stelio Henrique <steliohlopes@gmail.com>
2023-07-27 17:07:34 -0300

    [add] Fluid Class

--- ProblemInputs.py
+++ ProblemInputs.py
@@ -4,16 +4,6 @@ class Inputs:
         self.meshPath = "PreProcessing/Whistle/"
         self.meshFile = "whistle"
 
-        ## Rheology - Modified SMD (Souza Mendes e Dutra (2004)) + Cure(tauY(t))
-        # Input Variables
-        self.rho = 1000  # Density (kg/m³)
-        self.tau0 = 6.21358  # Dinamic Yield Stress
-        self.eta0 = 0.001  # Viscosity Value for Low shear rates
-        self.etaInf = 64.1  # Equilibrium Viscosity(Newtonian Plato: Lowgh shear rates)
-        self.k = 0.1  # Consistency Index
-        self.nPow = 0.572  # Power-law Index
-        self.ts = 663  # Caracteristic viscosity buildup time
-
         ## Solver Parameters
         self.nonlinearSolver = "newton"
         self.absTol = 1e-9
@@ -41,3 +31,38 @@ class Inputs:
         self.Uin = 0.0025
         self.inletBCs = []
         self.inletBCs.append("Inlet")
+
+class Fluid:
+    def __init__(self, rho=None, k=None, nPow=None, tau0=None, eta0=None, etaInf=None, ts=None):
+
+        ## Rheology - Modified SMD (Souza Mendes e Dutra (2004)) + Cure(tauY(t))
+        # Input Variables
+        self.rho = rho  # Density (kg/m³)
+        self.k = k  # Consistency Index
+        self.nPow = nPow  # Power-law Index
+        self.tau0 = tau0 # Dinamic Yield Stress
+        self.eta0 = eta0  # Viscosity Value for Low shear rates
+        self.etaInf = etaInf  # Equilibrium Viscosity(Newtonian Plato: Lowgh shear rates)
+        self.ts = ts  # Caracteristic viscosity buildup time
+
+    @classmethod
+    def from_file(cls, file_path):
+        #.txt file exemple
+
+        # rho=1000
+        # k=0.1
+        # nPow=0.572
+        # tau0=6.21358
+        # eta0=0.001
+        # etaInf=64.1
+        # ts=663
+
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+
+        attributes = {}
+        for line in lines:
+            key, value = line.strip().split('=')
+            attributes[key.strip()] = float(value.strip())
+
+        return cls(**attributes)
