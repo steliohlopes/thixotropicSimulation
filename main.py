e55e2c1c65b17f945801d1f2a6fc5270f956827c
Stelio Lopes <steliohlopes@gmail.com>
2023-08-10 12:38:03 +0000

    [fix] def inside Solver Class

--- main.py
+++ main.py
@@ -2,26 +2,18 @@ from PreProcessing.mesh import FiniteElementMesh
 from ProblemInputs import Fluid
 from Solver.boundaries import Boundaries
 from Solver.equations import Solver
-meshPath = "PreProcessing/PipeFlow/"
-meshFile = "PipeFlow"
+meshPath = "/home/lmmp/thixotropicSimulation/PreProcessing/ParallelPlates/"
+meshFile = "ParallelPlates"
 
 mesh = FiniteElementMesh(meshPath=meshPath,meshFile=meshFile)
 # mesh.msh2hdmf3D()
 print(mesh.subdomains)
 mesh.createMeshObject3D()
 
-boundaries = Boundaries(mesh=mesh, Pin=1,noSlipBCs=["Wall","Garganta"])
+boundaries = Boundaries(mesh=mesh, Pin=1)
 
-fluid = Fluid(
-        rho=1000,
-        k=0.1,
-        nPow=0.572,
-        tau0=6.21358,
-        eta0=0.001,
-        etaInf=64.1,
-        ts=663
-        )
+fluid = Fluid.from_file("/home/lmmp/thixotropicSimulation/PreProcessing/ParallelPlates/fluid.txt")
 
 newtonianTest = Solver(mesh=mesh,fluid=fluid,boundaries=boundaries)
-newtonianTest.PowerLawSolver()
-
+newtonianTest.NewtonianSolver()
+# newtonianTest.SaveSimulationData("ParallelPlates3D")
