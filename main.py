349394f319b7919d4c0eb6708454d2456264cbc1
Stelio Lopes <steliohlopes@gmail.com>
2023-08-31 15:59:05 +0000

    [add] tixotropy Values (tc,ta,S)

--- main.py
+++ main.py
@@ -3,32 +3,33 @@ from ProblemInputs import Fluid
 from Solver.boundaries import Boundaries
 from Solver.equations import Solver
 from Solver.problem import Problem
-meshPath = "/home/lmmp/thixotropicSimulation/PreProcessing/ParallelPlates/"
-meshFile = "ParallelPlates"
+meshPath = "/home/lmmp/thixotropicSimulation/PreProcessing/PipeFlow2D/"
+meshFile = "PipeFlow2D"
 
 mesh = FiniteElementMesh(meshPath=meshPath,meshFile=meshFile)
 # mesh.msh2hdmf3D()
 print(mesh.subdomains)
-mesh.createMeshObject3D()
+mesh.createMeshObject2D()
 
-boundaries = Boundaries(mesh=mesh, Pin=1,noSlipBCs=["BottomWall","TopWall","SideWall"])
+boundaries = Boundaries(mesh=mesh, Pin=1)
 
 fluid = Fluid(
         rho=1000,
         k=0.1,
-        nPow=0.572,
+        nPow=0.7,
         tau0=6.21358,
         eta0=0.001,
         etaInf=64.1,
         ts=663
         )
 problem = Problem(mesh=mesh,fluid=fluid,boundaries=boundaries)
-problem.NewtonianEquation()
-newtonianTest = Solver(problem)
-newtonianTest.SimulateEquation()
+# problem.NewtonianEquation()
+# newtonianTest = Solver(problem)
+# newtonianTest.SimulateEquation()
 
-problem.PowerLawEquation()
-newtonianTest.SimulateEquation()
-newtonianTest.SaveSimulationData(filePath=meshPath,fileName="ParallelPlatesNewtonian")
-
+# problem.PowerLawEquation()
+# newtonianTest.SimulateEquation()
+problem.ThixotropicEquation()
+tixotropicTest = Solver(problem)
+# newtonianTest.SaveSimulationData(filePath=meshPath,fileName="ParallelPlatesNewtonian")
 
