5d19b8fad6556cb4cfbacc694a0c21e847cd861a
Stelio Lopes <steliohlopes@gmail.com>
2024-01-09 19:53:48 +0000

    [fix] functions name change

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ boundaries = Boundaries(mesh=mesh, Pin=1e5)
 fluid = Fluid(
         rho=1000,
         k=1,
-        nPow=0.6,
+        nPow=0.60,
         phi0=0.001,
         phiInf=15,
         Ta = 1e-2,
@@ -29,42 +29,13 @@ fluid = Fluid(
         )
 problem = Problem(mesh=mesh,fluid=fluid,boundaries=boundaries)
 
-problem.NewtonianEquation()
+problem.GNFEquation('newtonian')
 newtonianTest = Solver(problem)
 newtonianTest.SimulateEquation()
-# newtonianTest.SaveSimulationData(filePath=meshPath,fileName="ContractionNewtonian")
 
-# problem.PowerLawEquation()
-# PowerLawTest = Solver(problem,maxIter = 100)
-# PowerLawTest.SimulateEquation()
-# PowerLawTest.SaveSimulationData(filePath=meshPath,fileName="ContractionPowerLaw")
-
-problem.boundaries.change_parameter(Fluidityin=0.1)
-
-problem.ThixotropicEquation()
-tixotropicTest = Solver(problem,maxIter = 100)
-tixotropicTest.SimulateEquation()
-tixotropicTest.SaveSimulationData(filePath=meshPath,fileName="ContractionThixotropic")
-tixotropicTest.velocity_plot(R=300e-6,xpoint= 23e-3/2,filePath=meshPath)
-problem.fluid.nPow-=0.05
-
-while problem.fluid.nPow>0.3:
-        problem.ThixotropicEquation()
-        tixotropicTest = Solver(problem,maxIter = 100)
-        tixotropicTest.SimulateEquation()
-        tixotropicTest.SaveSimulationData(filePath=meshPath,fileName="ContractionThixotropic")
-        tixotropicTest.velocity_plot(R=300e-6,xpoint= 23e-3/2,filePath=meshPath)
-        problem.fluid.nPow-=0.01
-
-
-
-# tixotropicTest.SaveSimulationData(filePath=meshPath,fileName="ContractionThixotropic")
-# tixotropicTest.velocity_plot(R=300e-6,xpoint= 23e-3/2,filePath=meshPath)
-
-# problem.fluid.nPow=0.30
-# problem.ThixotropicEquation()
-# tixotropicTest = Solver(problem,absTol=1.5e-12,maxIter = 100)
-# tixotropicTest.SimulateEquation()
-# tixotropicTest.SaveSimulationData(filePath=meshPath,fileName="ContractionThixotropic")
-
+problem.GNFEquation('SMD')
+PowerLawTest = Solver(problem,maxIter = 10000)
+PowerLawTest.SimulateEquation()
+PowerLawTest.SaveSimulationData(filePath=meshPath,fileName="ContractionSMD")
+PowerLawTest.velocity_plot(R=300e-6,xpoint= 23e-3/2,filePath=meshPath)
 
