e9b63ad16ff31160ef2fae37eee87f30fbb8bd15
Stelio Henrique <steliohlopes@gmail.com>
2024-01-23 15:51:04 -0300

    [add] DoF

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ if comm.rank ==0:
                 
 mesh.createMeshObject3D()
 
+info("Num DOFs {}".format(mesh.DoF))
+
 boundaries = Boundaries(mesh=mesh, Pin=1e5)
 
 fluid = Fluid(
@@ -26,7 +28,7 @@ fluid = Fluid(
         k=1,
         nPow=0.60,
         phi0=0.001,
-        phiInf=15,
+        phiInf=60,
         Ta = 1,
         Tc = 1
         )
@@ -35,13 +37,13 @@ problem = Problem(mesh=mesh,fluid=fluid,boundaries=boundaries)
 problem.GNFEquation('newtonian')
 newtonianTest = Solver(problem)
 newtonianTest.SimulateEquation()
-newtonianTest.SaveSimulationData(filePath=meshPath,fileName="PipeFlow3DNewtonian")
+newtonianTest.SaveSimulationData(filePath=meshPath,fileName="CoatingBarNewtonian")
 # newtonianTest.viscoty_plot(arrayx=[-4e-3,4e-3],fileName=f'{meshPath}NewtonianViscosity.png')
 
-# problem.GNFEquation('SMD')
-# PowerLawTest = Solver(problem,maxIter = 100)
-# PowerLawTest.SimulateEquation()
-# PowerLawTest.SaveSimulationData(filePath=meshPath,fileName="PipeFlow2DSMD")
+problem.GNFEquation('SMD')
+PowerLawTest = Solver(problem,maxIter = 100)
+PowerLawTest.SimulateEquation()
+PowerLawTest.SaveSimulationData(filePath=meshPath,fileName="CoatingBarSMD")
 # PowerLawTest.velocity_plot(R=100e-6,xpoint= 23e-3/2,fileName=f'{meshPath}SMDResult.png')
 # PowerLawTest.viscoty_plot(arrayx=[-4e-3,4e-3],fileName=f'{meshPath}GNFViscosity.png')
 
@@ -49,6 +51,6 @@ boundaries.change_parameter(Fluidityin=0.1)
 problem.ThixotropicEquation()
 Thixotropic = Solver(problem,maxIter = 100)
 Thixotropic.SimulateEquation()
-Thixotropic.SaveSimulationData(filePath=meshPath,fileName="PipeFlow3DThixotropic")
+Thixotropic.SaveSimulationData(filePath=meshPath,fileName="CoatingBarThixotropic")
 # Thixotropic.velocity_plot(R=100e-6,xpoint= 23e-3/2,fileName=f'{meshPath}ThixotropicResultTa{problem.fluid.Ta}.png')
 # Thixotropic.viscoty_plot(arrayx=[-4e-3,4e-3],fileName=f'{meshPath}ThixotropicViscosity{problem.fluid.Ta}.png')
