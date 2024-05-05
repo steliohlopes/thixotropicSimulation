b620677ec4f0b024703adb3709f549f9d0486942
Stelio Henrique <steliohlopes@gmail.com>
2024-05-05 03:20:56 -0300

    [test] contraction results

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ import os
 
 comm = MPI.comm_world
 
-meshPath = "/home/stelio/thixotropicSimulation/PreProcessing/ParallelPlatesSymmetry/"
-meshFile = "ParallelPlatesSymmetry"
-simulation_type = '3D'
+meshPath = "/home/stelio/thixotropicSimulation/PreProcessing/Contraction/"
+meshFile = "Contraction"
+simulation_type = '2D'
 
 mesh = FiniteElementMesh(meshPath=meshPath,meshFile=meshFile)
 
@@ -35,40 +35,40 @@ fluid = Fluid(
         k=1,
         nPow=0.8,
         phi0=0.001,
-        phiInf=2,
-        Ta = 1,
-        Tc = 1
+        phiInf=20,
+        Ta = 50,
+        Tc = 50
         )
 
 Pin = 2000
 Pout = 0
-L = 2
-H=0.2
+L = 24e-3
+H=100e-6*2
 U = -((Pout-Pin)/L)*(H/2/(2*fluid.k))*(H-H/2)
 
-boundaries = Boundaries(mesh=mesh, Pin=Pin,Pout=Pout,symmetryBCs=["Symmetry"],symmetryAxis=2)
+if comm.rank ==0:
+        info("L characteristic {}".format(L))
+        info("U characteristic {}".format(U))
+
+boundaries = Boundaries(mesh=mesh, Pin=Pin,Pout=Pout)
 
 problem = Problem(mesh=mesh,fluid=fluid,boundaries=boundaries,U = U, L=L)
 
 problem.GNFEquation('newtonian')
 newtonianTest = Solver(problem)
 newtonianTest.SimulateEquation()
-newtonianTest.SaveSimulationData(filePath=meshPath,fileName="ParallelPlatesSymmetryNewtonian")
+newtonianTest.SaveSimulationData(filePath=meshPath,fileName="ContractionNewtonian")
 wini = problem.w
 del newtonianTest
 
-problem.GNFEquation('SMD')
-PowerLawTest = Solver(problem,absTol = 1e-6)
-PowerLawTest.SimulateEquation()
-PowerLawTest.SaveSimulationData(filePath=meshPath,fileName="ParallelPlatesSymmetrySMD")
 
-# boundaries.change_parameter(Fluidityin=0.1)
-# problem2 = Problem(mesh=mesh,fluid=fluid,boundaries=boundaries)
-# problem2.ThixotropicEquation(wini=wini)
+boundaries.change_parameter(Fluidityin=0.01)
+problem2 = Problem(mesh=mesh,fluid=fluid,boundaries=boundaries,U = U, L=L)
+problem2.GNFEquation(wini=wini,model='thixotropic')
 # del wini
-# Thixotropic = Solver(problem2,maxIter = 100,absTol = 1e-6)
-# Thixotropic.SimulateEquation()
-# Thixotropic.SaveSimulationData(filePath=meshPath,fileName="CoatingBarSymmetrytinyThixotropicLaponite")
+Thixotropic = Solver(problem2,maxIter = 100,absTol = 1e-6)
+Thixotropic.SimulateEquation()
+Thixotropic.SaveSimulationData(filePath=meshPath,fileName="ContractionThixotropic",dimensional=True)
 # wini = problem2.w
 # del Thixotropic, problem2
 
