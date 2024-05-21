67c239528781e7705833779383723418649db4ae
Stelio Henrique <steliohlopes@gmail.com>
2024-05-21 16:54:36 -0300

    [main] 2D Constricted with Uin BC

--- main.py
+++ main.py
@@ -8,21 +8,23 @@ import os
 
 comm = MPI.comm_world
 
-meshPath = "/home/stelio/thixotropicSimulation/PreProcessing/CoatingHangerSymmetry2/"
-meshFile = "CoatingHangerSymmetry2"
-simulation_type = '3D'
+meshPath = "/home/stelio/thixotropicSimulation/PreProcessing/Constricted/"
+meshFile = "Constricted"
+simulation_type = '2D'
 
-Pin = 3000
+# Pin = 3000
 Pout = 0
 W_outlet=10e-3
-L=W_outlet/2
+# L=W_outlet/2
 # H=100e-6*2
 # U = -((Pout-Pin)/L)*(H/2/(2*fluid.k))*(H-H/2)
-U=1e-3
 
-sweep_dict={0:0.005,1:-0.0001,2:[0,0.005]}
-velocity_coord = 0
-num_points = 3000
+L = 24e-3
+U=1e-2
+Pinf=10000
+# sweep_dict={0:0.005,1:-0.0001,2:[0,0.005]}
+# velocity_coord = 0
+# num_points = 3000
 
 
 mesh = FiniteElementMesh(meshPath=meshPath,meshFile=meshFile)
@@ -58,51 +60,23 @@ if comm.rank ==0:
         info("L characteristic {}".format(L))
         info("U characteristic {}".format(U))
 
-boundaries = Boundaries(mesh=mesh, Pin=Pin,Pout=Pout,symmetryBCs=["Symmetry"],symmetryAxis=2)
+boundaries = Boundaries(mesh=mesh, UinVector=(1,0),Pout=Pout)
 
-problem = Problem(mesh=mesh,fluid=fluid,boundaries=boundaries,U = U, L=L)
+problem = Problem(mesh=mesh,fluid=fluid,boundaries=boundaries,U = U, L=L,Pinf=Pinf)
 
 problem.Equation('newtonian')
 newtonianTest = Solver(problem)
 newtonianTest.SimulateEquation()
-newtonianTest.SaveSimulationData(filePath=meshPath,fileName="CoatingHangerSymmetry2Newtonian",dimensional=True)
-newtonianTest.velocity_plot(sweep_dict=sweep_dict,velocity_coord=velocity_coord,num_points=num_points,fileName="CoatingHangerSymmetry2Newtonian.png")
+newtonianTest.SaveSimulationData(filePath=meshPath,fileName="ConstrictedNewtonian",dimensional=True)
+# newtonianTest.velocity_plot(sweep_dict=sweep_dict,velocity_coord=velocity_coord,num_points=num_points,fileName="CoatingHangerSymmetry2Newtonian.png")
 wini = problem.w
 del newtonianTest
 
 boundaries.change_parameter(Fluidityin=0.1)
-problem2 = Problem(mesh=mesh,fluid=fluid,boundaries=boundaries,U = U, L=L)
+problem2 = Problem(mesh=mesh,fluid=fluid,boundaries=boundaries,U = U, L=L,Pinf=Pinf)
 problem2.Equation(wini=wini,model='thixotropic')
-
-times = [5,1,0.1,0.01,0.001,0.0001]
 
 Thixotropic = Solver(problem2,maxIter = 100,absTol = 1e-6)
 Thixotropic.SimulateEquation()
-Thixotropic.SaveSimulationData(filePath=meshPath,fileName=f"CoatingHangerSymmetry2Thixotropic{problem2.fluid.Ta}",dimensional=True)
-Thixotropic.velocity_plot(sweep_dict=sweep_dict,velocity_coord=velocity_coord,num_points=num_points,fileName=f"CoatingHangerSymmetry2Thixotropic{problem2.fluid.Ta}.png")
-wini = problem2.w
-del Thixotropic
-
-for t in times:
-        fluid.Ta=t
-        fluid.Tc=t
-        problem = Problem(mesh=mesh,fluid=fluid,boundaries=boundaries,U = U, L=L)
-        problem.Equation(wini=wini,model='thixotropic')
-        Thixotropic = Solver(problem,maxIter = 100,absTol = 1e-6)
-        try:
-                Thixotropic.SimulateEquation()
-                Thixotropic.SaveSimulationData(filePath=meshPath,fileName=f"CoatingHangerSymmetry2Thixotropic{t}",dimensional=True)
-                Thixotropic.velocity_plot(sweep_dict=sweep_dict,velocity_coord=velocity_coord,num_points=num_points,fileName=f"CoatingHangerSymmetry2Thixotropic{t}.png")
-                wini = problem.w
-                del Thixotropic
-        except:
-                pass
-
-del boundaries
-boundaries = Boundaries(mesh=mesh, Pin=Pin,Pout=Pout,symmetryBCs=["Symmetry"],symmetryAxis=2)  
-problem = Problem(mesh=mesh,fluid=fluid,boundaries=boundaries,U = U, L=L)
-problem.Equation(wini=wini,model='SMD')
-Thixotropic = Solver(problem,maxIter = 100,absTol = 1e-6)
-Thixotropic.SimulateEquation()
-Thixotropic.SaveSimulationData(filePath=meshPath,fileName=f"CoatingHangerSymmetry2SMD",dimensional=True)
-Thixotropic.velocity_plot(sweep_dict=sweep_dict,velocity_coord=velocity_coord,num_points=num_points,fileName="CoatingHangerSymmetry2SMD.png")
+Thixotropic.SaveSimulationData(filePath=meshPath,fileName=f"ConstrictedThixotropic{problem2.fluid.Ta}",dimensional=True)
+# Thixotropic.velocity_plot(sweep_dict=sweep_dict,velocity_coord=velocity_coord,num_points=num_points,fileName=f"CoatingHangerSymmetry2Thixotropic{problem2.fluid.Ta}.png")
